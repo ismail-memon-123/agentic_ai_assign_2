f43493619a66f0962c09f478cb528c76e1a4b5d6
Ismail Memon <ismail.memon@vanderbilt.edu>
2026-07-03 17:07:34 -0700

    Update bedrock_llm.py

--- bedrock_llm.py
+++ bedrock_llm.py
@@ -1,23 +1,3 @@
-from planner import plan_loop
-
-from bedrock_llm import (
-    bedrock_llm_call
-)
-
-question = """
-What is the mean of order_total?
-Use the CSV preview and compute_stats.
-"""
-
-answer = plan_loop(
-    bedrock_llm_call,
-    question,
-    session_id="session1"
-)
-
-print("\nFINAL ANSWER")
-print(answer)
-[ec2-user@ip-172-31-35-181 mcp-server]$ cat bedrock_llm.py
 from crewai import LLM
 
 llm = LLM(
